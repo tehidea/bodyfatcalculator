755f1506b5e9115da2cf057d40c4f5671c8bb29d
Michał Ordon <michal@tehidea.com>
2024-12-02 01:38:49 +0000

    Add splash

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ThemeProvider } from "@rneui/themed";
@@ -15,29 +15,49 @@ import { theme } from "./src/constants/theme";
 import { initializeStore } from "./src/config/store";
 import { View } from "react-native";
 
-// Keep splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded, fontError] = useFonts({
     "Montserrat-ExtraLight": Montserrat_200ExtraLight,
     "Montserrat-Light": Montserrat_300Light,
   });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
   useEffect(() => {
-    initializeStore().catch(console.error);
+    async function prepare() {
+      try {
+        // Initialize any resources, load data, etc.
+        await initializeStore();
+
+        // Artificially delay for two seconds to simulate a slow loading
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
 
-  if (!fontsLoaded && !fontError) {
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && (fontsLoaded || fontError)) {
+      // This tells the splash screen to hide immediately
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, fontsLoaded, fontError]);
+
+  // Don't render anything until the app is ready
+  if (!appIsReady || (!fontsLoaded && !fontError)) {
     return null;
   }
 
