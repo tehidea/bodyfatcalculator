75119093ab5ea2b62f3eca78119b1bb5e2ac5b13
Michał Ordon <michal@tehidea.com>
2024-12-04 18:25:36 +0000

    Fix icons

--- src/images/icons/MeasuringTapeIcon.tsx
+++ src/images/icons/MeasuringTapeIcon.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react'
 
-export function MeasuringTapeIcon({ className }: { className?: string }) {
+export function MeasuringTapeIcon({
+  className,
+  size = '122.88',
+  color = 'currentColor',
+}: {
+  className?: string
+  size?: string
+  color?: string
+}) {
   return (
     <svg
       viewBox="0 0 122.88 122.88"
       className={className}
       xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
     >
       <path
-        fill="currentColor"
+        fill={color}
         d="M61.44 0c33.93 0 61.44 27.51 61.44 61.44s-27.51 61.44-61.44 61.44S0 95.37 0 61.44 27.51 0 61.44 0Zm-1.7 34.23h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm-8.42-36.19h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm16.84-36.19h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Zm0 14.48h5.06v5.06h-5.06v-5.06Z"
       />
     </svg>
