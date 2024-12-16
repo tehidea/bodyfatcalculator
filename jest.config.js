d7a390b4d0888405c75c825ea00e6283df62a502
Michał Ordon <michal@tehidea.com>
2024-12-16 03:40:31 +0000

    Add test coverage script

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   preset: "jest-expo",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "clover"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/*.spec.{ts,tsx}",
+    "!src/**/__tests__/**",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
