9e49fb645c7aa6b3413a6ea62dfd98596d9036ae
Michał Ordon <michal@tehidea.com>
2024-12-16 01:30:49 +0000

    Tests be gone

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   preset: "jest-expo",
-  setupFiles: ["./jest.setup.js"],
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-  ],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "**/*.{ts,tsx}",
-    "!**/coverage/**",
-    "!**/node_modules/**",
-    "!**/babel.config.js",
-    "!**/jest.setup.js",
-  ],
-  moduleNameMapper: {
-    "^@react-native-async-storage/async-storage$":
-      "<rootDir>/src/__mocks__/@react-native-async-storage/async-storage.ts",
-  },
 };
